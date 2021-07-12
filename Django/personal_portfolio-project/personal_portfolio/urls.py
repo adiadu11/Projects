1bb9e9a2e73684b31cb6ad437f470fcf0ee8048c
unknown <adityabalodi.adu@gmail.com>
2021-07-12 10:22:04 +0530

    Adding templates

--- Django/personal_portfolio-project/personal_portfolio/urls.py
+++ Django/personal_portfolio-project/personal_portfolio/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('projects', views.home),
+    path('projects/<int:project_id>', views.detail, name='detail')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Django/personal_portfolio-project/portfolio/views.py
+++ Django/personal_portfolio-project/portfolio/views.py
@@ -1,3 +1,14 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from .models import Project
 # Create your views here.
+
+
+def home(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/home.html', {'projects': projects})
+
+
+def detail(request, project_id):
+    project = Project.objects.get(pk=project_id)
+    return render(request, 'portfolio/detail.html', {'project': project})
